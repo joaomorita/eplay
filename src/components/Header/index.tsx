3055be971e74491f108a04a239b634a176bff53f
Morita <joao@teste.com>
2025-04-23 09:40:11 -0300

    cria cabecalho

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+import { HeaderBar, Links, LinkItem, LinkCart } from './styles'
+
+import logo from '../../assets/logo.svg'
+import carrinho from '../../assets/carrinho.svg'
+
+const Header = () => (
+  <HeaderBar>
+    <div>
+      <img src={logo} alt="EPLAY Logo" />
+      <nav>
+        <Links>
+          <LinkItem>
+            <a href="#">Categorias</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Novidades</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Promoções</a>
+          </LinkItem>
+        </Links>
+      </nav>
+    </div>
+    <LinkCart href="#">
+      0 - Produto(s)
+      <img src={carrinho} alt="Carrinho de compras" />
+    </LinkCart>
+  </HeaderBar>
+)
+
+export default Header
